401a139159b6ef8c8bce730ff475d8f520e76654
dengzh <dengzhui00@163.com>
2019-07-24 16:30:06 +0800

    Initial commit

---
+++ app/src/main/java/com/sz/dzh/dviewsummary/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sz.dzh.dviewsummary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
